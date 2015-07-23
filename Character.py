dd5a59f42779238765af971e314d905d3075b792
Guema <floorent1993@gmail.com>
2015-07-23 11:59:38 +0200

    Un pas de plus vers la modularité ^.^

--- Character.py
+++ Character.py
@@ -1,4 +1,96 @@
 import cocos
+from cocos import tiles, actions
+from pyglet.window import key
+
+
+class PlayerController(actions.Action, tiles.RectMapCollider):
+    on_ground = True
+    MOVE_SPEED = 200
+    JUMP_SPEED = 500
+    GRAVITY = -1500
+
+    def start(self):
+        # initial velocity
+        self.target.velocity = (0, 0)
+
+    def step(self, dt):
+        # import singleton
+        from Singleton import keyboard, tilemap
+        dx, dy = self.target.velocity
+
+        # using the player controls, gravity and other acceleration influences
+        # update the velocity
+        dx = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * self.MOVE_SPEED * dt
+        dy = dy + self.GRAVITY * dt
+        if self.on_ground and keyboard[key.SPACE]:
+            dy = self.JUMP_SPEED
+
+        # get the player's current bounding rectangle
+        last = self.target.get_rect()
+        new = last.copy()
+        new.x += dx
+        new.y += dy * dt
+
+        # run the collider
+        dx, dy = self.target.velocity = self.collide_map(tilemap,
+                                                         last,
+                                                         new,
+                                                         dy,
+                                                         dx)
+        self.on_ground = bool(new.y == last.y)
+
+        # player position is anchored in the center of the image rect
+        self.target.position = new.center
+
+        # move the scrolling view to center on the player
+        # scroller.set_focus(*new.center)
+
+
+class BasicEnnemyController(actions.Action, tiles.RectMapCollider):
+    on_ground = True
+    MOVE_SPEED = 100
+    JUMP_SPEED = 500
+    GRAVITY = -1500
+
+    def start(self):
+        # initial velocity
+        self.target.velocity = (0, 0)
+        self.GoLeft = 1
+        self.GoRight = 0
+        self.GoJump = False
+
+    def step(self, dt):
+        # import singleton
+        from Singleton import tilemap
+        dx, dy = self.target.velocity
+
+        # using the player controls, gravity and other acceleration influences
+        # update the velocity
+        dx = (self.GoRight - self.GoLeft) * self.MOVE_SPEED * dt
+        dy = dy + self.GRAVITY * dt
+        if self.on_ground and self.GoJump:
+            dy = self.JUMP_SPEED
+            cocos.actions.Rotate
+
+        # get the player's current bounding rectangle
+        last = self.target.get_rect()
+        new = last.copy()
+        new.x += dx
+        new.y += dy * dt
+
+        # run the collider
+        dx, dy = self.target.velocity = self.collide_map(tilemap,
+                                                         last,
+                                                         new,
+                                                         dy,
+                                                         dx)
+        self.on_ground = bool(new.y == last.y)
+
+        # player position is anchored in the center of the image rect
+        self.target.position = new.center
+
+        # move the scrolling view to center on the player
+        # scroller.set_focus(*new.center)
 
 
 class Character(cocos.sprite.Sprite):
@@ -20,6 +112,7 @@ class Player(Character):
     def __init__(self, path):
         super(Player, self).__init__(path)
         # this statement call super constructor
+        self.do(PlayerController())
 
 
 class IACharacter(Character):
@@ -32,3 +125,4 @@ class IACharacter(Character):
     def __init__(self, path):
         super(IACharacter, self).__init__(path)
         # this statement call super constructor
+        self.do(BasicEnnemyController())
--- Launcher.py
+++ Launcher.py
@@ -2,8 +2,8 @@ import pyglet
 from pyglet.window import key
 
 import cocos
-from cocos import tiles, actions, layer
-import Character
+from cocos import layer
+from Character import *
 
 # Singleton init
 
@@ -11,98 +11,8 @@ pyglet.resource.path.append(pyglet.resource.get_script_home())
 pyglet.resource.reindex()
 
 
-class PlayerController(actions.Action, tiles.RectMapCollider):
-    on_ground = True
-    MOVE_SPEED = 200
-    JUMP_SPEED = 500
-    GRAVITY = -1500
-
-    def start(self):
-        # initial velocity
-        self.target.velocity = (0, 0)
-
-    def step(self, dt):
-        # import singleton
-        from Singleton import keyboard, tilemap
-        dx, dy = self.target.velocity
-
-        # using the player controls, gravity and other acceleration influences
-        # update the velocity
-        dx = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * self.MOVE_SPEED * dt
-        dy = dy + self.GRAVITY * dt
-        if self.on_ground and keyboard[key.SPACE]:
-            dy = self.JUMP_SPEED
-
-        # get the player's current bounding rectangle
-        last = self.target.get_rect()
-        new = last.copy()
-        new.x += dx
-        new.y += dy * dt
-
-        # run the collider
-        dx, dy = self.target.velocity = self.collide_map(tilemap,
-                                                         last,
-                                                         new,
-                                                         dy,
-                                                         dx)
-        self.on_ground = bool(new.y == last.y)
-
-        # player position is anchored in the center of the image rect
-        self.target.position = new.center
-
-        # move the scrolling view to center on the player
-        # scroller.set_focus(*new.center)
-
-
-class BasicEnnemyController(actions.Action, tiles.RectMapCollider):
-    on_ground = True
-    MOVE_SPEED = 100
-    JUMP_SPEED = 500
-    GRAVITY = -1500
-
-    def start(self):
-        # initial velocity
-        self.target.velocity = (0, 0)
-        self.GoLeft = 1
-        self.GoRight = 0
-        self.GoJump = False
-
-    def step(self, dt):
-        # import singleton
-        from Singleton import tilemap
-        dx, dy = self.target.velocity
-
-        # using the player controls, gravity and other acceleration influences
-        # update the velocity
-        dx = (self.GoRight - self.GoLeft) * self.MOVE_SPEED * dt
-        dy = dy + self.GRAVITY * dt
-        if self.on_ground and self.GoJump:
-            dy = self.JUMP_SPEED
-            cocos.actions.Rotate
-
-        # get the player's current bounding rectangle
-        last = self.target.get_rect()
-        new = last.copy()
-        new.x += dx
-        new.y += dy * dt
-
-        # run the collider
-        dx, dy = self.target.velocity = self.collide_map(tilemap,
-                                                         last,
-                                                         new,
-                                                         dy,
-                                                         dx)
-        self.on_ground = bool(new.y == last.y)
-
-        # player position is anchored in the center of the image rect
-        self.target.position = new.center
-
-        # move the scrolling view to center on the player
-        # scroller.set_focus(*new.center)
-
-
 def main():
-    import Singleton
+    import Singleton  # importation en écriture
     from cocos.director import director
     director.init(width=1024, height=768, autoscale=False)
 
@@ -111,12 +21,12 @@ def main():
     character_layer = layer.ScrollableLayer()
     # NOTE: the anchor for this sprite is in the CENTER (the cocos default)
     # which means all positioning must be done using the center of its rect
-    player = cocos.sprite.Sprite('sprites/units/characterM83.png')
-    ennemy = cocos.sprite.Sprite('sprites/units/character.png')
+    player = Player('sprites/units/characterM83.png')
+    ennemy = IACharacter('sprites/units/character.png')
     character_layer.add(player)
     character_layer.add(ennemy)
-    player.do(PlayerController())
-    ennemy.do(BasicEnnemyController())
+    # player.do(PlayerController())
+    # ennemy.do(BasicEnnemyController())
 
     # add the tilemap and the player sprite layer to a scrolling manager
     Singleton.scroller = layer.ScrollingManager()
